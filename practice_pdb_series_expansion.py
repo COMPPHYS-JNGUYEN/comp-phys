b85236cadbf451d7a890599a03f82c4e47d52f1c
COMPPHYS-JNGUYEN <jqnguyen@usfca.edu>
2015-09-10 15:42:19 -0700

    Second commit

--- practice_pdb_series_expansion.py
+++ practice_pdb_series_expansion.py
@@ -5,17 +5,21 @@ from math import log1p  #more accurate for small x.
 
 def L(x, n):
     approx = 0.
-    #pdb.set_trace()
+    approx_prev = 0.
     if x <= -1:
         print "Cannot use given value"
         return
+    for i in range(0, n+1):
+        approx_prev = approx
+        approx += (1/(i+1.))*(x/(1.+x))**(i+1.)
+    if (approx - approx_prev)/approx > 10**(-6):
+        print "Returned value is not within desried error.  Increase n."
+        return approx
     else:
-        for i in range(0, n+1):
-            approx += (1/(i+1.))*(x/(1.+x))**(i+1.)
         return approx
 
 
-x = 1
+x = 2
 y = L(x, 100)
 print 'Taylor Series Approximation:', y
 exact_val = log(1+x)

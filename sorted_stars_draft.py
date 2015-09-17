97999749c9ad35c3125dd8b3fa575093c0394267
COMPPHYS-JNGUYEN <jqnguyen@usfca.edu>
2015-09-17 13:54:35 -0700

    Attempt to print lists side by side.

--- sorted_stars_draft.py
+++ sorted_stars_draft.py
@@ -4,15 +4,39 @@
 import pprint
 
 def sortXvsY(data, index1=0, index2=0):
+	"""
+		Function takes data and sorts the data according to which indices are desired.  For example, in the variable data2 below are data pertaining to star names, distance from the Sun
+		in light years, apparent brightness according to how bright it is in our sky compared to Sirius A, and absolute brightness-- how bright the stars would look if all were the same
+		distance as the Sun, compared with the Sun.  The program takes the data, and sorts it alphabetically if desired by name.  If distance is desired, indicate the corresponding indices
+		and the program will sort the data according to Name vs. (Desired), e.g. Name vs. Apparent Brightness.
+
+>>> sortXvsY(data2)
+Sorted data according to index 0.
+<BLANKLINE>
+[('Alpha Centauri A', 4.3, 0.26, 1.56),
+ ('Alpha Centauri B', 4.3, 0.077, 0.45),
+ ('Alpha Centauri C', 4.2, 1e-05, 6e-05),
+ ('BD +36 degrees 2147', 8.2, 0.0003, 0.006),
+ ("Barnard's Star", 6.0, 4e-05, 0.0005),
+ ('Luyten 726-8 A', 8.4, 3e-06, 6e-05),
+ ('Luyten 726-8 B', 8.4, 2e-06, 4e-05),
+ ('Ross 154', 9.4, 2e-05, 0.0005),
+ ('Sirius A', 8.6, 1.0, 23.6),
+ ('Sirius B', 8.6, 0.001, 0.003),
+ ('Wolf 359', 7.7, 1e-06, 2e-05)]
+<BLANKLINE>
+>>> sortXvsY(data2, 4, 1)
+Cannot use provided indices.  Max index is 3.
+	"""
 	if index1 or index2 >= len(data[0]):		# Wanted to generalize function given different data, but same format.  If either index is out of range, print message and stop remainder of program.
 		LenDataElement = len(data[0])			# Length of element in data, used to establish how high index can go.
 		print 'Cannot use provided indices.  Max index is {}.'.format(LenDataElement - 1)	# Prints out error message, stating that index maxes out at len(data[0]).
 		return									# Exits out of function
 	elif index1 == index2:						# If indices equal to each other after passing first (if) condition, then sort data according to index2 (although using either index doesn't matter since both are equal).						
 		print 'Sorted data according to index {}.\n'.format(index2)							# A notice to user that because indices matched, sorted data by default indices or equal indices.
-		pprint.pprint(sorted(data, key = lambda x: x[index2]))								# Sort data, and pretty print, according to index2.
+		x = pprint.pprint(sorted(data, key = lambda x: x[index2]))								# Sort data, and pretty print, according to index2.
 		print									# Empty line to make printing look nicer.
-		return sorted(data, key = lambda x: x[index2])
+		return x
    
 	data = sorted(data, key = lambda x: x[index2])											# Sort data according to given index, index2.
 	X = []										# Empty list for first desired list, used for initialization.
@@ -49,8 +73,11 @@ data2 = [
 ('Ross 154',            9.4,    0.00002,    0.0005),
 ]
 
-sortXvsY(data2)
 sortXvsY(data2, 0, 1)
-sortXvsY(data2, 0, 2)
-sortXvsY(data2, 0, 3)
-sortXvsY(data2, 4, 1)
+#sortXvsY(data2, 0, 2)
+#sortXvsY(data2, 0, 3)
+
+
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()

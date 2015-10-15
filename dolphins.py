bcc3932c822c5f3c56fe96f172ffa066f7079b08
COMPPHYS-JNGUYEN <jqnguyen@usfca.edu>
2015-10-14 21:32:00 -0700

    So progress, much wow

--- dolphins.py
+++ dolphins.py
@@ -2,87 +2,92 @@ import random
 import numpy
 import re
 import urllib
+from pdb import set_trace
 
 #####################################################################################################################
 
+# malenames and femalenames initialized such that it extracts information from URLs and puts information into lists.
+# Put above functions and other code to be used as global variable inside functions.
 malenames = []
 femalenames = []
 
-def findmalenames(code):
-    matchstring = 'nameDetails">.*</span>'
-    namelist = []
-    for item in code:
-        x = re.findall(matchstring, item)
-        if len(x) > 0:
-            namelist.append(x[0])
-    for stuff in namelist:
-        new_stuff = stuff.replace('nameDetails">', "")
-        newnew_stuff = new_stuff.replace('</span>', "")
-        malenames.append(newnew_stuff)
+# creates the text file directory path for the male and female lists for later extraction.
+# put above functions and other code to be used as global variable inside functions.
+male_dir_path = '/Users/labuser/comp-phys/'         # PC: "C:/Users/James/Documents/GitHub/comp-phys/"
+male_filenm = male_dir_path + 'malenames.txt'       # Mac: "/Users/labuser/comp-phys/"
+female_dir_path = '/Users/labuser/comp-phys/'
+female_filenm = female_dir_path + 'femalenames.txt'
 
-def findfemalenames(code):
+def findnames(code, gender):
     matchstring = 'nameDetails">.*</span>'
-    namelist = []
-    for item in code:
-        x = re.findall(matchstring, item)
-        if len(x) > 0:
-            namelist.append(x[0])
-    for stuff in namelist:
-        new_stuff = stuff.replace('nameDetails">', "")
-        newnew_stuff = new_stuff.replace('</span>', "")
-        femalenames.append(newnew_stuff)
-        
+    if gender == 'M':
+        namelist = []
+        for item in code:
+            x = re.findall(matchstring, item)
+            if len(x) > 0:
+                namelist.append(x[0])
+        for stuff in namelist:
+            new_stuff = stuff.lstrip('nameDetails">').rstrip('</span>')
+            malenames.append(new_stuff)
+    else:
+        namelist = []
+        for item in code:
+            x = re.findall(matchstring, item)
+            if len(x) > 0:
+                namelist.append(x[0])
+        for stuff in namelist:
+            new_stuff = stuff.lstrip('nameDetails">').rstrip('</span>')
+            femalenames.append(new_stuff)
+
 def namegen(sex):
     x = 0
     y = 0
     if sex == 'M':
-        with open(male_path,"r") as f:
+        with open(male_filenm,"r") as f:
             malenames = eval(f.read())
-            print malenames
         while x < len(malenames)-1:
             yield malenames[x]
             x += 1
     else:
-        with open(female_path, "r") as f:
+        with open(female_filenm, "r") as f:
             femalenames = eval(f.read())
         while y < len(femalenames):
             yield femalenames[y]
             y += 1
 
-
+# Extracts male and female names from URL.
 i = 1
 maleurl = "http://www.prokerala.com/kids/baby-names/boy/page-1.html"
 femaleurl = "http://www.prokerala.com/kids/baby-names/girl/page-1.html"
-while len(malenames) < 7880:    #7880
+while len(malenames) < 1000:    #Male Names Max: 7880
     x = maleurl.replace('1', str(i))
     xinfile = urllib.urlopen(x)
     xhtml = xinfile.readlines()
     xinfile.close()
-    findmalenames(xhtml)
+    findnames(xhtml, 'M')
     i += 1
 
 j = 1
-while len(femalenames) < 5974:    #5974
+while len(femalenames) < 1000:    # Female Names Max: 5974
     y = femaleurl.replace('1', str(j))
     yinfile = urllib.urlopen(y)
     yhtml = yinfile.readlines()
     yinfile.close()
-    findfemalenames(yhtml)
+    findnames(yhtml, 'F')
     j += 1
-    
-male_dir_path = "C:/Users/James/Documents/GitHub/comp-phys/"      # "/Users/labuser/comp-phys/" for mac
-male_filenm = male_dir_path + 'malenames.txt'
+
+# Make male and female names list extracted from websites text file that is writable.
 with open(male_filenm, "w") as f:
     f.write(str(malenames))
-    
-female_dir_path = "C:/Users/James/Documents/GitHub/comp-phys/"    # "/Users/labuser/comp-phys/" for mac
-female_filenm = female_dir_path + 'femalenames.txt'
 with open(female_filenm, "w") as f:
     f.write(str(femalenames))
-    
+
+#####################################################################################################################
 #####################################################################################################################
 
-child_dict = {}
+# Set male and female generator equal to namegen() with respective genders to make use of .next() inside function.
+malegenerator = namegen('M')
+femalegenerator = namegen('F')
 
 def MarvinGaye(partner1, partner2):
     if partner1.sex == 'M':
@@ -95,12 +100,19 @@ def MarvinGaye(partner1, partner2):
         FemalePartner = partner2.name
     if partner1.legal(partner2) == True:
         child_sex = random.sample(['M', 'F'], 1)[0]
-        child_name = namegen(child_sex)
-        child_dict[child_name] = Dolphins(child_name, child, FemalePartner, MalePartner)
-        partner1.refracperiod = 0
-        partner2.refracperiod = 0
-        
-        
+        if child_sex == 'M':
+            child_name = malegenerator.next()
+            dolphinstances[child_name] = Dolphins(child_name, child_sex, FemalePartner, MalePartner)
+            partner1.refracperiod = 0
+            partner2.refracperiod = 0
+        else:
+            child_name = femalegenerator.next()
+            dolphinstances[child_name] = Dolphins(child_name, child_sex, FemalePartner, MalePartner)
+            partner1.refracperiod = 0
+            partner2.refracperiod = 0
+        return 1
+
+
 class Dolphins:
     def __init__(self, name, sex, mother, father):
         self.name = name
@@ -110,12 +122,14 @@ class Dolphins:
         self.father = father
         self.death = random.gauss(35, 5)
         self.refracperiod = 0
-        
-    def agify(self):                                 ## Aging method: updates the age attribute each year.
+    
+## Aging method: updates the age attribute each year.
+    def agify(self):
         self.age += 1
         self.refracperiod += 1
-        
-    def legal(self, partner):                   ## Procreation method: determines whether a dolphin is allowed to procreate with another.
+
+## Procreation method: determines whether a dolphin is allowed to procreate with another.
+    def legal(self, partner):
         if (self.age >= 8)\
         and (partner.age >= 8)\
         and (abs(self.age - partner.age) <= 10)\
@@ -123,19 +137,51 @@ class Dolphins:
         and (self.father != partner.father)\
         and (self.mother != partner.mother)\
         and (self.refracperiod > 5)\
-        and (partner.refracperiod > 5):
+        and (partner.refracperiod > 5)\
+        and (self.age <= self.death)\
+        and (partner.age <= partner.death):
             proc = True
+            return proc
         else:
             proc = False
-        return proc
+            return proc
 
-    
-########################
-##### Main Program #####
-########################
-
+# Instantiate elder dolphins (with arbitrary parents).
 elder_dolphins = ['Shakira', 'Jency', 'Lothar', 'JinBiao']
 dolphinstances = {elder_dolphins[0]: Dolphins(elder_dolphins[0], 'F', 'Jen', 'Sven'),\
-                  elder_dolphins[1]: Dolphins(elder_dolphins[1], 'F', 'Jan', 'Stan'),\
-                  elder_dolphins[2]: Dolphins(elder_dolphins[2], 'M', 'June', 'Stoon'),\
-                  elder_dolphins[3]: Dolphins(elder_dolphins[3], 'M', 'Jill', 'Skrill')}
+    elder_dolphins[1]: Dolphins(elder_dolphins[1], 'F', 'Jan', 'Stan'),\
+    elder_dolphins[2]: Dolphins(elder_dolphins[2], 'M', 'June', 'Stoon'),\
+    elder_dolphins[3]: Dolphins(elder_dolphins[3], 'M', 'Jill', 'Skrill')}
+
+#####################################################################################################################
+#####################################################################################################################
+#####################################################################################################################
+
+years = 0
+breeding = 0
+deaths = []
+while years < 76:
+    temp1 = dolphinstances.keys()
+    print len(temp1)
+    for dolphin in temp1:
+        dolphinstances[dolphin].agify()
+        for partner in temp1:
+            cool = MarvinGaye(dolphinstances[dolphin], dolphinstances[partner])
+            if cool == 1:
+                breeding += cool
+    for elder in temp1:
+        if dolphinstances[elder].age >= dolphinstances[elder].death:
+            if elder not in deaths:
+                deaths.append(elder)
+    set_trace()
+    temp2 = dolphinstances.keys()
+    print len(temp2)
+    if years % 25 == 0:
+        print "#"*50
+        print "Entering year {:d} with {:d} dolphins, with {:d} breeding.".format(years, len(temp1)-len(deaths), abs(len(temp2)-len(temp1)))
+    if years == 100:
+        print "At year {:d}, there are {:d} living dolphins.\nthere have been {:d} births, in total.".format(years, len(temp2)-len(deaths), len(temp2)-4)
+    if years == 149:
+        print "#"*50
+        print "At year {:d}, there are {:d} living dolphins.".format(years, len(temp2)-len(deaths))
+    years += 1

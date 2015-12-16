38ce9c04563b1f1d451254054e7c212492fa9f93
COMPPHYS-JNGUYEN <jqnguyen@usfca.edu>
2015-12-16 01:42:49 -0800

    stuff

--- pattern_recog_func.py
+++ pattern_recog_func.py
@@ -2,17 +2,31 @@ from scipy.interpolate import interp2d
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from skimage import transform, data, io
 
 def interpol_im(im, dim1=8, dim2=8, plot_new_im=False, cmap='binary', axis_off=False):
-    f2d = interp2d(dim1, dim2, im)
+    im = im[:,:,0]
+    x = np.arange(im.shape[1])
+    y = np.arange(im.shape[0])
+    print(x.shape,y.shape)
+    f2d = interp2d(x, y, im)
     
-    if plt_new_im:
-        plt.imshow(f2d, cmap=cmap, interpolation='nearest')
+    x_new = np.linspace(0, im.shape[1], dim1)
+    y_new = np.linspace(0, im.shape[0], dim2)
+    
+    im_new = f2d(x_new, y_new)
+    
+    im_flat = im_new.flatten()
+    
+    if plot_new_im:
+        plt.imshow(im_new, cmap=cmap, interpolation='nearest')
         plt.grid('off')
         plt.show()
         
-    f2d_flat = f2d.flatten()
-    return f2d_flat
+    return im_flat
 
 def pca_X(X, n_comp=10):
     md_pca = PCA(n_comp)
@@ -22,9 +36,26 @@ def pca_X(X, n_comp=10):
 def rescale_pixel(X, unseen, ind=0):
     dig_data = load_digits()
     X[ind] = dig_data.data
-
-# def pca_svm_pred(imfile, md_pca, md_clf, dim1=45, dim2=60):
-#     im = mpimg.imread(imfile)
-#     f2d_flat = interpol_im(im, dim1=dim1, dim2=dim2)
     
-#     clf = svm.SVC(kernel='linear')
+    return
+    
+def svm_train(X, y, gamma=0.001, C=100):
+    
+    return
+
+def pca_svm_pred(imfile, md_pca, md_clf, dim1=45, dim2=60):
+    im = mpimg.imread(imfile)
+    f2d_flat = interpol_im(im, dim1=dim1, dim2=dim2)
+    return
+
+# letterB = mpimg.imread('letterB.png')
+# letterB = letterB[:,:,0]
+
+
+# dig_data = load_digits()
+# X = dig_data.data
+# y = dig_data.target
+
+# print(letterB.shape)
+# f2d = interpol_im(letterB, plot_new_im=True)
+# # print(np.mean(X[0]), np.mean(letterB))
